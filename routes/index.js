70a24f43270dac502c8b4b72e494baa0e0a73f00
muhammedfayiscc <mohammed.fayis@pegasusone.com>
2024-01-21 16:19:50 +0530

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const { getRoomsDB } = require('../libs/db/users')
+
 const userRouter = require('./users');
 const invalidRequestHandler = require('../libs/middleware/invalidRequestHandler');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.status(200).json({
-        success:true,
-        title: 'Nodejs Task Project Test '
-    });
+router.get('/', async function (req, res, next) {
+    let data = await getRoomsDB();
+    if(data) {
+        res.status(200).json({
+            success:true,
+            datas:data.data,
+            title: 'Nodejs Task Project Test '
+        });
+    }
 });
 router.use(invalidRequestHandler);
 router.use('/user', userRouter);
